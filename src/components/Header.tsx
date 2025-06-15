9aca85502742d917799e978a951541ade2aac1c5
Pranjal1804 <mitrapranjal2004@gmail.com>
2025-06-15 12:55:04 +0530

    name changed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <div className="header">
       <h1>
-        <i className="fas fa-satellite-dish"></i> TactMap
+        <i className="fas fa-satellite-dish"></i> CosmoMap
       </h1>
       <div className="subtitle">Real-time Object Detection on Satellite Imagery</div>
     </div>
